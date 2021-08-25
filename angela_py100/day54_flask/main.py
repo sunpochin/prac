c316c2bc164dd245d5e272a89c91f3aa5ae6c48a
sun pochin <sunpochin@gmail.com>
2021-08-25 22:02:09 +0800

    adding day58 tindog

--- angela_py100/day54_flask/main.py
+++ angela_py100/day54_flask/main.py
@@ -3,27 +3,30 @@ import requests
 
 app = Flask(__name__)
 
+url = "https://api.npoint.io/ed99320662742443cc5b"
+response = requests.get(url)
+print("response: ", response)
+posts = response.json()
+print("posts: ", posts)
+
 # posts = ""
 @app.route('/')
 def home():
-    url = "https://api.npoint.io/ed99320662742443cc5b"
-    response = requests.get(url)
-    print("response: ", response)
-    posts = response.json()
-    print("posts: ", posts)
     return render_template("index.html", posts=posts)
 
 
 @app.route('/blog/<num>')
 def get_post(num):
-    url = "https://api.npoint.io/ed99320662742443cc5b"
-    response = requests.get(url)
-    # print("response: ", response)
-    posts = response.json()
-    idx = int(num)
-    post = posts[idx - 1]
-    print("idx: ", idx, ", posts: ", posts, "\n, post: ", post)
-    return render_template("post.html", num=num, post=post)
+
+    idx = int(num) - 1
+    requested_post = None
+    for post in posts:
+        print("num: ", num, ", type: ", type(num), ", post id: ", post["id"], ", post id type: ", type(post["id"]) )
+        if str(post["id"]) == str(num):
+            requested_post = post
+            print("requested_post: ", requested_post)
+    print("idx: ", idx, ", posts: ", posts, "\n, requested_post: ", requested_post)
+    return render_template("post.html", num=num, post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
